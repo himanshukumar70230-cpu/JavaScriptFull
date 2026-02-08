82007dab1b9214435f29ba2c15e811278ee8d64f
HIMANSHU KUMAR <himanshukumar70230@gmail.com>
2026-02-08 21:50:26 +0530

    BasicJs

---
+++ basic.js
@@ -0,0 +1,11 @@
+console.log("Himanshu"); //const is unchangeable
+const id = 2345
+let accountEmail = "Himansh"
+var accountPassword = "123"
+accountCity = "Lakhisarai"
+//id = 2
+console.log(id);
+console.table([id,accountEmail,accountPassword,accountCity])
+/*  Don't use var
+ because of issue in block scope and fuctional scope*/
+        
---
+++ dataType.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all jscode as new version
+//alert("hello") we are using nodeJs not browser
+console.log(3+3); console.log("Himanshu") //this make bad readability of code
+let name = "Himasnhu"
+let age = 21
+let isLoggedIn = false
+console.table([name,age,isLoggedIn]);
+/**int number 2to power 53
+ * boolean true or false
+ * bigint for large number
+ * string " " or '' can we written in single or double Quto
+ * null standalone value or undefined
+ * symbol use for unquieness
+ */
+console.log(typeof age)
