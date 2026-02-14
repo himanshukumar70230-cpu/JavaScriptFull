064c4d5c9b2bf02743992935dd53be5bf7cc6568
HIMANSHU KUMAR <himanshukumar70230@gmail.com>
2026-02-14 18:42:45 +0530

    dataType_js

--- dataType.js
+++ dataType.js
@@ -1,4 +1,4 @@
-"use strict"; //treat all jscode as new version
+//"use strict"; //treat all jscode as new version
 //alert("hello") we are using nodeJs not browser
 console.log(3+3); console.log("Himanshu") //this make bad readability of code
 let name = "Himasnhu"
@@ -7,9 +7,31 @@ let isLoggedIn = false
 console.table([name,age,isLoggedIn]);
 /**int number 2to power 53
  * boolean true or false
- * bigint for large number
+ * bigint for large number = bigNumber
  * string " " or '' can we written in single or double Quto
  * null standalone value or undefined
  * symbol use for unquieness
+ * 
  */
-console.log(typeof age)
+
+const hero = ["shaktiman","naagraj","doga"];
+let myObj= {
+    name : "Himanshu",
+    age: 22
+}
+const myFunction = function(){
+    console.log("Hello world")
+
+}
+console.log(typeof hero);
+console.log(typeof null)
+//type of val = result
+let undefined = "undefined"
+let NULL = "object"
+let Boolean = "Boolean"
+let Number = "number"
+let string = "string";
+console.log(myFunction) //all non primitive data type called an object
+const id = Symbol('123')
+console.log(typeof id)
+
