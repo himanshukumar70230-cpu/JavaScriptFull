cf638c769ca9a2a1ba0159384e6add4646b93c6f
HIMANSHU KUMAR <himanshukumar70230@gmail.com>
2026-02-25 00:44:14 +0530

    Basic of js

--- basic.js
+++ basic.js
@@ -1,11 +1,21 @@
-console.log("Himanshu"); //const is unchangeable
-const id = 2345
-let accountEmail = "Himansh"
-var accountPassword = "123"
-accountCity = "Lakhisarai"
-//id = 2
-console.log(id);
-console.table([id,accountEmail,accountPassword,accountCity])
-/*  Don't use var
- because of issue in block scope and fuctional scope*/
-        
+console.log("Himanshu Good Evening");
+var a = 10;
+var b = 20;
+console.log(a+b);
+var c = "Himanshu"
+console.log(typeof a,typeof b,typeof c);
+//let  is Blocked variable
+let d = 65;
+{
+    let d = 35;
+    console.log(d);
+}
+ console.log(d);
+
+//var is Global variable
+var e = 55;
+{
+    var e = 45;
+    console.log(e);
+} 
+ console.log(e);
