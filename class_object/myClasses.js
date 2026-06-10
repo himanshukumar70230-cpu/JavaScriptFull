a5f4fcdc9d05da5428805bbe77911a8ba1c8fdf6
HIMANSHU KUMAR <himanshukumar70230@gmail.com>
2026-06-11 01:36:06 +0530

    updated class

--- class_object/myClasses.js
+++ class_object/myClasses.js
@@ -19,3 +19,20 @@ const chai = new user('himanshu','chai@gmail.com',"123");
 console.log(chai.encryptPassword());
 
 console.log(chai.changeUsername());
+
+
+//behind the scene
+
+function User(username,email,password){
+    this.username = username;
+    this.email = email;
+    this.password = password;
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+
+const tea = new user('himanshutea','tea@gmail.com',"123");
+
+console.log(tea.encryptPassword());
